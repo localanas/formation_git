70632cbc4dddcd37f01b8bafb7da547569627eac
localanas <anas.nadi10@gmail.com>
2026-02-26 20:26:11 +0000

    ci/cd + testing

---
+++ demo_laravel/tests/Unit/CalculatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Services\Calculator;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_addition()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(5, $calc->add(2, 3));
+    }
+
+    public function test_subtraction()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(1, $calc->subtract(3, 2));
+    }
+
+}
